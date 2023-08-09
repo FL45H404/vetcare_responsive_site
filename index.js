863c901c6e407bafa67eb1a8c577b5d8cb89e3a9
FL45H404 <vipulwaghamode194@gmail.com>
2023-08-09 20:23:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const btn=document.querySelector('.mobile-nav-btn')
+const header=document.querySelector('.header')
+btn.addEventListener('click',function(){
+    header.classList.toggle('active')
+
+})
+
+$('.slick-carousel').slick({
+    infinite: true,
+    slidesToShow: 3, // Shows a three slides at a time
+    slidesToScroll: 1, // When you click an arrow, it scrolls 1 slide at a time
+    arrows: false, // Adds arrows to sides of slider
+    dots: false, // Adds the dots on the bottom
+    autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+  })
+  $('.slick-carousel1').slick({
+    infinite: true,
+    slidesToShow: 3, // Shows a three slides at a time
+    slidesToScroll: 1, // When you click an arrow, it scrolls 1 slide at a time
+    arrows: false, // Adds arrows to sides of slider
+    dots: false, // Adds the dots on the bottom
+    autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+
+  ]
+  })
